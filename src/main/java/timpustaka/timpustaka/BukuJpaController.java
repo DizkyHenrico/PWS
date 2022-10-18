8b2f9b66f539f9c411f1ce8713cc81cecf2bac2a
DizkyHenrico <henricozdzky@gmail.com>
2022-10-18 18:27:35 +0700

    add persistance

--- src/main/java/timpustaka/timpustaka/BukuJpaController.java
+++ src/main/java/timpustaka/timpustaka/BukuJpaController.java
@@ -15,6 +15,7 @@ import java.util.Collection;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import timpustaka.timpustaka.exceptions.IllegalOrphanException;
 import timpustaka.timpustaka.exceptions.NonexistentEntityException;
 import timpustaka.timpustaka.exceptions.PreexistingEntityException;
@@ -28,7 +29,7 @@ public class BukuJpaController implements Serializable {
     public BukuJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("timpustaka_timpustaka_jar_0.0.1-SNAPSHOTPU");
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
